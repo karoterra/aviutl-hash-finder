e539aa30c75230140d8cebb8b13c5a767bfbffcf
karoterra <karoterra2008@gmail.com>
2022-08-14 19:23:13 +0900

    :wrench: デプロイ用の設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/aviutl-hash-finder/" : "./",
   plugins: [vue()],
   resolve: {
     alias: {
